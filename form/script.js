bb56990c53f16ed8660139934ed74e4f5f498e4c
abdulloh-azizovich <81867645+abdulloh-azizovich@users.noreply.github.com>
2021-05-20 23:09:59 +0500

    Add files via upload

---
+++ form/script.js
@@ -0,0 +1,15 @@
+let cont = document.querySelector('.demo');
+let html = ``;
+
+function send(){
+    let info = [ ]
+
+    let name = document.querySelector('.name');
+    let password = document.querySelector('.password');
+    info.pop = name.value
+    info.push = password.value
+
+
+    console.log(password.value);
+}
+
